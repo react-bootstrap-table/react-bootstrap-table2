1575b3abb43f320b08c7a64f99676a564f9706fd
AllenFang <ayu780129@hotmail.com>
2017-08-20 16:35:46 +0800

    implement column formatter

--- packages/react-bootstrap-table2/src/row.js
+++ packages/react-bootstrap-table2/src/row.js
@@ -3,18 +3,25 @@ import PropTypes from 'prop-types';
 
 import Cell from './cell';
 
-const Row = ({ row, columns }) => (
+const Row = ({ row, rowIndex, columns }) => (
   <tr>
     {
       columns.map(column =>
-        <Cell key={ row[column.dataField] } value={ row[column.dataField] } />
-      )
+        (
+          <Cell
+            key={ row[column.dataField] }
+            row={ row }
+            rowIndex={ rowIndex }
+            column={ column }
+          />
+        ))
     }
   </tr>
 );
 
 Row.propTypes = {
   row: PropTypes.object.isRequired,
+  rowIndex: PropTypes.number.isRequired,
   columns: PropTypes.array.isRequired
 };
 
