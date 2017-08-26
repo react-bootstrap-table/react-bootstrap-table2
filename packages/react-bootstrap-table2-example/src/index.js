4bd3fa1d3d83e2326c9365cce9550d3ad3e08459
AllenFang <ayu780129@hotmail.com>
2017-08-26 15:02:06 +0800

    implement column.headerTitle

--- packages/react-bootstrap-table2-example/src/index.js
+++ packages/react-bootstrap-table2-example/src/index.js
@@ -3,8 +3,64 @@ import ReactDom from 'react-dom';
 
 import { BootstrapTable } from 'react-bootstrap-table2';
 
-const data = [1, 2, 3, 4];
+const products = [];
+
+function addProducts(quantity) {
+  const startId = products.length;
+  for (let i = 0; i < quantity; i += 1) {
+    const id = startId + i;
+    products.push({
+      id,
+      name: `Item name ${id}`,
+      price: 2100 + i,
+      nest: {
+        address: 'Address 1',
+        postcal: '0922-1234'
+      }
+    });
+  }
+}
+
+addProducts(5);
+
+const columns = [{
+  dataField: 'id',
+  text: 'Product ID',
+  style: {
+    backgroundColor: 'red'
+  },
+  headerTitle: (column, colIndex) => {
+    console.log(column);
+    console.log(colIndex);
+    return 'yes~~~ oh';
+  },
+  classes: 'my-xxx'
+}, {
+  dataField: 'name',
+  text: 'Product Name',
+  headerTitle: true,
+  formatter: (cell, row) =>
+    (<h3>{ cell }::: ${ row.price }</h3>)
+}, {
+  dataField: 'price',
+  text: 'Product Price',
+  style: (cell, row, colIndex) => {
+    console.log(cell);
+    console.log(row);
+    console.log(colIndex);
+    return {
+      backgroundColor: 'blue'
+    };
+  }
+}, {
+  dataField: 'nest.address',
+  text: 'Address'
+}, {
+  dataField: 'nest.postcal',
+  text: 'Postal'
+}];
+
 
 ReactDom.render(
-  <BootstrapTable data={ data } />,
+  <BootstrapTable keyField="id" data={ products } columns={ columns } />,
   document.getElementById('example'));
--- packages/react-bootstrap-table2/src/header-cell.js
+++ packages/react-bootstrap-table2/src/header-cell.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import _ from './utils';
 
-const HeaderCell = ({ column }) => (
-  <th>
-    { column.text }
-  </th>
-);
+
+const HeaderCell = ({ column, index }) => {
+  const { headerTitle, text } = column;
+  const attrs = {};
+
+  if (headerTitle) {
+    attrs.title = _.isFunction(headerTitle) ? headerTitle(column, index) : text;
+  }
+
+  return (
+    <th { ...attrs }>
+      { column.text }
+    </th>
+  );
+};
 
 HeaderCell.propTypes = {
   column: PropTypes.shape({
@@ -15,8 +26,10 @@ HeaderCell.propTypes = {
     formatter: PropTypes.func,
     formatExtraData: PropTypes.any,
     classes: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
-    style: PropTypes.oneOfType([PropTypes.object, PropTypes.func])
-  }).isRequired
+    style: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+    headerTitle: PropTypes.oneOfType([PropTypes.bool, PropTypes.func])
+  }).isRequired,
+  index: PropTypes.number.isRequired
 };
 
 export default HeaderCell;
