562a0d67ab84d8a29e8e8ab4cd06960153698582
Chun-MingChen <nick830314@gmail.com>
2017-08-27 21:19:36 +0800

    import bootstrap css for storybook
    * update webpack.config for adding loader for font and css

--- packages/react-bootstrap-table2-example/.storybook/webpack.config.js
+++ packages/react-bootstrap-table2-example/.storybook/webpack.config.js
@@ -20,9 +20,15 @@ const loaders = [{
   exclude: /node_modules/,
   include: [sourcePath, storyPath],
 }, {
+  test: /\.css$/,
+  use: ['style-loader', 'css-loader'],
+}, {
   test: /\.scss$/,
-  use: ['style-loader', 'raw-loader', 'sass-loader'],
-  include: [storyPath],
+  use: ['style-loader', 'css-loader', 'sass-loader'],
+  include: [storyPath, sourcePath, examplesPath],
+}, {
+  test: /\.(jpg|png|woff|woff2|eot|ttf|svg)$/,
+  loader: 'url-loader?limit=100000',
 }];
 
 // Export a function. Accept the base config as the only param.
--- packages/react-bootstrap-table2-example/stories/index.js
+++ packages/react-bootstrap-table2-example/stories/index.js
@@ -1,11 +1,13 @@
 /* eslint import/no-unresolved: 0 */
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
 import { linkTo } from '@storybook/addon-links';
 
 import { Welcome } from '@storybook/react/demo';
 import BasicTable from 'examples/basic/index.js';
+
+// css style
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
