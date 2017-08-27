a5ac6020415e36d8f8169c4d3c97ccb4d7d024dc
Chun-MingChen <nick830314@gmail.com>
2017-08-28 00:05:33 +0800

    wrapper components with Decorators to set component margin

--- packages/react-bootstrap-table2-example/.storybook/config.js
+++ packages/react-bootstrap-table2-example/.storybook/config.js
@@ -1,9 +1,21 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
-
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
 
 function loadStories() {
   require('stories');
 }
+
+const styles = {
+  margin: '15px',
+};
+
+const componentDecorator = (story) => (
+  <div style={styles}>
+    { story() }
+  </div>
+);
+
+addDecorator(componentDecorator);
 
 configure(loadStories, module);
