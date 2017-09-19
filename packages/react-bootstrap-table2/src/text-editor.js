80828567dbea29c89a65a608bd3a355acd4bb706
khanin_daniil <khanin_daniil@mail.ru>
2017-09-19 14:54:48 +0300

    fix  focus on the Last Character in Text Input

--- packages/react-bootstrap-table2/src/text-editor.js
+++ packages/react-bootstrap-table2/src/text-editor.js
@@ -3,16 +3,23 @@ import React, { Component } from 'react';
 
 class TextEditor extends Component {
   componentDidMount() {
+    const { defaultValue } = this.props;
+    this.text.value = defaultValue;
     this.text.focus();
   }
 
   render() {
+    const {
+      onKeyDown,
+      onBlur
+    } = this.props;
     return (
       <input
         ref={ node => this.text = node }
         type="text"
         className="form-control editor edit-text"
-        { ...this.props }
+        onKeyDown={onKeyDown}
+        onBlur={onBlur}
       />
     );
   }
