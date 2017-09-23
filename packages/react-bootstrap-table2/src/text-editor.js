ba7c2e9bb7ed493eb05fda844bb321aee30548bd
Daniil Khanin <khanin_daniil@mail.ru>
2017-09-23 09:15:01 +0300

    Allow focus on the last character in Text Input (#65)

--- packages/react-bootstrap-table2/src/text-editor.js
+++ packages/react-bootstrap-table2/src/text-editor.js
@@ -1,21 +1,31 @@
 /* eslint no-return-assign: 0 */
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class TextEditor extends Component {
   componentDidMount() {
+    const { defaultValue } = this.props;
+    this.text.value = defaultValue;
     this.text.focus();
   }
 
   render() {
+    const { defaultValue, ...rest } = this.props;
     return (
       <input
         ref={ node => this.text = node }
         type="text"
         className="form-control editor edit-text"
-        { ...this.props }
+        { ...rest }
       />
     );
   }
 }
 
+TextEditor.propTypes = {
+  defaultValue: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]).isRequired
+};
 export default TextEditor;
