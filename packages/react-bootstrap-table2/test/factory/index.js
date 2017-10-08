2a7ec721d756e74e8f28ccfbee39c64424782151
Chun-MingChen <nick830314@gmail.com>
2017-10-08 16:14:21 +0800

    add mock data factory to generate base props

--- packages/react-bootstrap-table2/test/factory/index.js
+++ packages/react-bootstrap-table2/test/factory/index.js
@@ -1,6 +1,19 @@
 import { bodyResolvedProps, headerResolvedProps } from './resolved-props';
+import {
+  baseColumns, baseColumn, baseData,
+  baseKeyField, baseRow, baseProps
+} from './base-props';
 
+/**
+ * Provide the base props in order to test easiler.
+ */
 export {
   bodyResolvedProps,
-  headerResolvedProps
+  headerResolvedProps,
+  baseColumns,
+  baseColumn,
+  baseData,
+  baseRow,
+  baseKeyField,
+  baseProps
 };
