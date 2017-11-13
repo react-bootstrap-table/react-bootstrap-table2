b992f8fe3e646dd504d575c5adf6422a2d52b2f3
AllenFang <ayu780129@hotmail.com>
2017-11-13 23:14:17 +0800

    add tests for pagination

---
+++ packages/react-bootstrap-table2-paginator/test/page-resolver.test.js
@@ -0,0 +1,416 @@
+import React, { Component } from 'react';
+import { shallow } from 'enzyme';
+
+import pageResolver from '../src/page-resolver';
+
+const extendTo = Base =>
+  class MockComponent extends Base {
+    constructor(props) {
+      super(props);
+      this.state = this.initialState();
+    }
+    render() { return null; }
+  };
+
+describe('PageResolver', () => {
+  const ExtendBase = pageResolver(Component);
+  const MockComponent = extendTo(ExtendBase);
+
+  const createMockProps = () => ({
+    dataSize: 100,
+    sizePerPageList: [10, 20, 30, 50],
+    currPage: 1,
+    currSizePerPage: 10,
+    pageStartIndex: 1,
+    paginationSize: 5,
+    withFirstAndLast: true,
+    firstPageText: '<<',
+    prePageText: '<',
+    nextPageText: '>',
+    lastPageText: '>>',
+    alwaysShowAllBtns: false
+  });
+
+  let wrapper;
+
+  describe('initialize', () => {
+    beforeEach(() => {
+      const mockElement = React.createElement(MockComponent, createMockProps(), null);
+      wrapper = shallow(mockElement);
+    });
+
+    it('should creating initial state correctly', () => {
+      const instance = wrapper.instance();
+      expect(instance.state.totalPages).toBeDefined();
+      expect(instance.state.totalPages).toEqual(instance.calculateTotalPage());
+      expect(instance.state.lastPage).toBeDefined();
+      expect(instance.state.lastPage).toEqual(
+        instance.calculateLastPage(instance.state.totalPages));
+      expect(instance.state.dropdownOpen).toBeDefined();
+      expect(instance.state.dropdownOpen).toBeFalsy();
+    });
+  });
+
+  describe('backToPrevPage', () => {
+    const props = createMockProps();
+
+    describe('when props.currPage is not hitting props.pageStartIndex', () => {
+      beforeEach(() => {
+        props.currPage = 2;
+        const mockElement = React.createElement(MockComponent, props, null);
+        wrapper = shallow(mockElement);
+      });
+
+      it('should getting previous page correctly', () => {
+        const instance = wrapper.instance();
+        expect(instance.backToPrevPage()).toEqual(props.currPage - 1);
+      });
+    });
+
+    describe('when props.currPage is hitting props.pageStartIndex', () => {
+      beforeEach(() => {
+        props.currPage = props.pageStartIndex;
+        const mockElement = React.createElement(MockComponent, props, null);
+        wrapper = shallow(mockElement);
+      });
+
+      it('should always getting page which must eq props.pageStartIndex', () => {
+        const instance = wrapper.instance();
+        expect(instance.backToPrevPage()).toEqual(props.pageStartIndex);
+      });
+    });
+  });
+
+  describe('goToNextPage', () => {
+    const props = createMockProps();
+
+    describe('when props.currPage is not hitting state.lastPage', () => {
+      beforeEach(() => {
+        const mockElement = React.createElement(MockComponent, props, null);
+        wrapper = shallow(mockElement);
+      });
+
+      it('should getting previous page correctly', () => {
+        const instance = wrapper.instance();
+        expect(instance.goToNextPage()).toEqual(props.currPage + 1);
+      });
+    });
+
+    describe('when props.currPage is hitting state.lastpage', () => {
+      beforeEach(() => {
+        props.currPage = 10;
+        const mockElement = React.createElement(MockComponent, props, null);
+        wrapper = shallow(mockElement);
+      });
+
+      it('should always getting page which must eq props.pageStartIndex', () => {
+        const instance = wrapper.instance();
+        expect(instance.goToNextPage()).toEqual(instance.state.lastPage);
+      });
+    });
+  });
+
+  describe('calculateTotalPage', () => {
+    const props = createMockProps();
+
+    describe('when missing sizePerPage argument', () => {
+      beforeEach(() => {
+        const mockElement = React.createElement(MockComponent, props, null);
+        wrapper = shallow(mockElement);
+      });
+
+      it('should getting total pages correctly by default props.currSizePerPage', () => {
+        const instance = wrapper.instance();
+        expect(instance.calculateTotalPage()).toEqual(10);
+      });
+    });
+
+    describe('when sizePerPage argument given', () => {
+      beforeEach(() => {
+        const mockElement = React.createElement(MockComponent, props, null);
+        wrapper = shallow(mockElement);
+      });
+
+      it('should getting total pages correctly by sizePerPage argument', () => {
+        const instance = wrapper.instance();
+        expect(instance.calculateTotalPage(25)).toEqual(4);
+      });
+    });
+  });
+
+  describe('calculateLastPage', () => {
+    beforeEach(() => {
+      const props = createMockProps();
+      const mockElement = React.createElement(MockComponent, props, null);
+      wrapper = shallow(mockElement);
+    });
+
+    it('should getting last page correctly', () => {
+      const instance = wrapper.instance();
+      expect(instance.calculateLastPage(instance.state.totalPages)).toEqual(10);
+    });
+  });
+
+  describe('calculatePages', () => {
+    describe('calculate by state.totalPages and state.lastPage', () => {
+      const props = createMockProps();
+      beforeEach(() => {
+        const mockElement = React.createElement(MockComponent, props, null);
+        wrapper = shallow(mockElement);
+      });
+
+      it('should getting pages list correctly', () => {
+        const instance = wrapper.instance();
+        expect(instance.calculatePages()).toEqual(
+          [props.prePageText, 1, 2, 3, 4, 5, props.nextPageText, props.lastPageText]);
+
+        expect(instance.calculatePages(4, 4)).toEqual(
+          [props.prePageText, 1, 2, 3, 4, props.nextPageText]);
+      });
+    });
+
+    describe('calculate by props.currPage', () => {
+      const props = createMockProps();
+      const { firstPageText, prePageText, nextPageText, lastPageText } = props;
+
+      it('should getting pages list correctly', () => {
+        const currPages = Array.from(Array(10).keys());
+        currPages.forEach((currPage) => {
+          props.currPage = currPage + 1;
+          wrapper = shallow(<MockComponent { ...props } />);
+          const pageList = wrapper.instance().calculatePages();
+
+          if (props.currPage < 4) {
+            expect(pageList).toEqual(
+              [prePageText, 1, 2, 3, 4, 5, nextPageText, lastPageText]);
+          } else if (props.currPage > 7) {
+            expect(pageList).toEqual(
+              [firstPageText, prePageText, 6, 7, 8, 9, 10, nextPageText]);
+          } else if (props.currPage === 4) {
+            expect(pageList).toEqual(
+              [firstPageText, prePageText, 2, 3, 4, 5, 6, nextPageText, lastPageText]);
+          } else if (props.currPage === 5) {
+            expect(pageList).toEqual(
+              [firstPageText, prePageText, 3, 4, 5, 6, 7, nextPageText, lastPageText]);
+          } else if (props.currPage === 6) {
+            expect(pageList).toEqual(
+              [firstPageText, prePageText, 4, 5, 6, 7, 8, nextPageText, lastPageText]);
+          } else {
+            expect(pageList).toEqual(
+              [firstPageText, prePageText, 5, 6, 7, 8, 9, nextPageText, lastPageText]);
+          }
+        });
+      });
+    });
+
+    describe('the quantity of pages is calculated by props.paginationSize', () => {
+      const props = createMockProps();
+      const indicators = [
+        props.firstPageText, props.prePageText, props.lastPageText, props.nextPageText
+      ];
+
+      it('should getting pages list correctly', () => {
+        [1, 3, 5, 8, 10].forEach((paginationSize) => {
+          props.paginationSize = paginationSize;
+          wrapper = shallow(<MockComponent { ...props } />);
+          const pageList = wrapper.instance().calculatePages();
+          const result = pageList.filter(p => indicators.indexOf(p) === -1);
+          expect(result.length).toEqual(props.paginationSize);
+        });
+      });
+    });
+
+    describe('when props.withFirstAndLast is true', () => {
+      const props = createMockProps();
+      describe('and last page is not visible by props.currPage', () => {
+        it('should getting pages list which contain last page indication', () => {
+          [1, 2, 3, 4, 5, 6, 7].forEach((currPage) => {
+            props.currPage = currPage;
+            wrapper = shallow(<MockComponent { ...props } />);
+            const pageList = wrapper.instance().calculatePages();
+            expect(pageList.indexOf(props.lastPageText) > -1).toBeTruthy();
+          });
+        });
+      });
+
+      describe('and first page is not visible by props.currPage', () => {
+        it('should getting pages list which contain first page indication', () => {
+          [10, 9, 8, 7, 6, 5, 4].forEach((currPage) => {
+            props.currPage = currPage;
+            wrapper = shallow(<MockComponent { ...props } />);
+            const pageList = wrapper.instance().calculatePages();
+            expect(pageList.indexOf(props.firstPageText) > -1).toBeTruthy();
+          });
+        });
+      });
+    });
+
+    describe('when props.withFirstAndLast is false', () => {
+      const props = createMockProps();
+      it('should not contain first and last page indication always', () => {
+        const currPages = Array.from(Array(10).keys());
+        currPages.forEach((currPage) => {
+          props.currPage = currPage + 1;
+          props.withFirstAndLast = false;
+          wrapper = shallow(<MockComponent { ...props } />);
+          const pageList = wrapper.instance().calculatePages();
+          expect(pageList.indexOf(props.lastPageText) > -1).toBeFalsy();
+          expect(pageList.indexOf(props.firstPageText) > -1).toBeFalsy();
+        });
+      });
+    });
+
+    describe('when props.pageStartIndex is negative number', () => {
+      const props = createMockProps();
+      props.pageStartIndex = -2;
+      props.currPage = -2;
+
+      beforeEach(() => {
+        const mockElement = React.createElement(MockComponent, props, null);
+        wrapper = shallow(mockElement);
+      });
+
+      it('should getting last page correctly', () => {
+        const pageList = wrapper.instance().calculatePages();
+        expect(pageList).toEqual(
+          [props.prePageText, -2, -1, 0, 1, 2, props.nextPageText, props.lastPageText]);
+      });
+    });
+
+    describe('when props.alwaysShowAllBtns is true', () => {
+      const props = createMockProps();
+      props.alwaysShowAllBtns = true;
+      props.currPage = 1;
+      props.dataSize = 11;
+
+      beforeEach(() => {
+        const mockElement = React.createElement(MockComponent, props, null);
+        wrapper = shallow(mockElement);
+      });
+
+      it('should always having next and previous page indication', () => {
+        const pageList = wrapper.instance().calculatePages();
+        expect(pageList.indexOf(props.nextPageText) > -1).toBeTruthy();
+        expect(pageList.indexOf(props.prePageText) > -1).toBeTruthy();
+      });
+    });
+
+    describe('when state.totalPages is zero', () => {
+      const props = createMockProps();
+      props.dataSize = 0;
+
+      beforeEach(() => {
+        const mockElement = React.createElement(MockComponent, props, null);
+        wrapper = shallow(mockElement);
+      });
+
+      it('should getting empty array', () => {
+        expect(wrapper.instance().calculatePages()).toEqual([]);
+      });
+    });
+  });
+
+  describe('calculatePageStatus', () => {
+    let instance;
+    let pageStatus;
+
+    describe('default case', () => {
+      const props = createMockProps();
+      beforeEach(() => {
+        const mockElement = React.createElement(MockComponent, props, null);
+        wrapper = shallow(mockElement);
+        instance = wrapper.instance();
+        pageStatus = instance.calculatePageStatus(instance.calculatePages());
+      });
+
+      it('should returning correct format for page status', () => {
+        pageStatus.forEach((p) => {
+          expect(Object.prototype.hasOwnProperty.call(p, 'page')).toBeTruthy();
+          expect(Object.prototype.hasOwnProperty.call(p, 'active')).toBeTruthy();
+          expect(Object.prototype.hasOwnProperty.call(p, 'disabled')).toBeTruthy();
+          expect(Object.prototype.hasOwnProperty.call(p, 'title')).toBeTruthy();
+        });
+      });
+
+      it('should mark active status as true when it is props.currPage', () => {
+        expect(pageStatus.find(p => p.page === props.currPage).active).toBeTruthy();
+      });
+
+      it('only have one page\'s active status is true', () => {
+        expect(pageStatus.filter(p => p.page === props.currPage).length).toEqual(1);
+      });
+    });
+
+    describe('when alwaysShowAllBtns is false', () => {
+      const props = createMockProps();
+      describe('and props.currPage is on first page', () => {
+        it('should filter out previous page indication', () => {
+          const mockElement = React.createElement(MockComponent, props, null);
+          wrapper = shallow(mockElement);
+          instance = wrapper.instance();
+          const pageList = instance.calculatePages();
+          pageStatus = instance.calculatePageStatus(pageList);
+
+          expect(pageStatus.find(p => p.page === props.prePageText)).not.toBeDefined();
+        });
+      });
+
+      describe('and props.currPage is on last page', () => {
+        it('should filter out next page indication', () => {
+          props.currPage = 10;
+          const mockElement = React.createElement(MockComponent, props, null);
+          wrapper = shallow(mockElement);
+          instance = wrapper.instance();
+          const pageList = instance.calculatePages();
+          pageStatus = instance.calculatePageStatus(pageList);
+
+          expect(pageStatus.find(p => p.page === props.nextPageText)).not.toBeDefined();
+        });
+      });
+    });
+  });
+
+  describe('calculateSizePerPageStatus', () => {
+    describe('when props.sizePerPageList is an number array', () => {
+      const props = createMockProps();
+      beforeEach(() => {
+        const mockElement = React.createElement(MockComponent, props, null);
+        wrapper = shallow(mockElement);
+      });
+
+      it('should getting correctly sizePerPage status', () => {
+        const instance = wrapper.instance();
+        const result = instance.calculateSizePerPageStatus();
+        expect(result.length).toEqual(props.sizePerPageList.length);
+        result.forEach((sizePerPage, i) => {
+          expect(sizePerPage.text).toEqual(`${props.sizePerPageList[i]}`);
+          expect(sizePerPage.page).toEqual(props.sizePerPageList[i]);
+        });
+      });
+    });
+
+    describe('when props.sizePerPageList is an object array', () => {
+      const props = createMockProps();
+      props.sizePerPageList = [{
+        text: 'ten', value: 10
+      }, {
+        text: 'thirty', value: 30
+      }];
+
+      beforeEach(() => {
+        const mockElement = React.createElement(MockComponent, props, null);
+        wrapper = shallow(mockElement);
+      });
+
+      it('should getting correctly sizePerPage status', () => {
+        const instance = wrapper.instance();
+        const result = instance.calculateSizePerPageStatus();
+        expect(result.length).toEqual(props.sizePerPageList.length);
+        result.forEach((sizePerPage, i) => {
+          expect(sizePerPage.text).toEqual(props.sizePerPageList[i].text);
+          expect(sizePerPage.page).toEqual(props.sizePerPageList[i].value);
+        });
+      });
+    });
+  });
+});
---
+++ packages/react-bootstrap-table2-paginator/test/wrapper.test.js
@@ -0,0 +1,320 @@
+import React from 'react';
+import sinon from 'sinon';
+import { shallow } from 'enzyme';
+
+
+import BootstrapTable from 'react-bootstrap-table2/src/bootstrap-table';
+import Store from 'react-bootstrap-table2/src/store/base';
+import paginator from '../src';
+import wrapperFactory from '../src/wrapper';
+import Pagination from '../src/pagination';
+
+const getConst = () => ({
+  PAGINATION_SIZE: 5,
+  PAGE_START_INDEX: 1,
+  With_FIRST_AND_LAST: true,
+  SHOW_ALL_PAGE_BTNS: false,
+  FIRST_PAGE_TEXT: '<<',
+  PRE_PAGE_TEXT: '<',
+  NEXT_PAGE_TEXT: '>',
+  LAST_PAGE_TEXT: '>>',
+  SIZE_PER_PAGE_LIST: [10, 25, 30, 50]
+});
+
+const data = [];
+for (let i = 0; i < 100; i += 1) {
+  data.push({
+    id: i,
+    name: `itme name ${i}`
+  });
+}
+
+describe('Wrapper', () => {
+  let PaginationWrapper;
+  let wrapper;
+  let instance;
+
+  const constants = getConst();
+  const createTableProps = props => ({
+    keyField: 'id',
+    columns: [{
+      dataField: 'id',
+      text: 'ID'
+    }, {
+      dataField: 'name',
+      text: 'Name'
+    }],
+    data,
+    pagination: paginator,
+    store: new Store({ data }),
+    ...props
+  });
+
+  const pureTable = props => (<BootstrapTable { ...props } />);
+
+  describe('default pagination', () => {
+    const props = createTableProps();
+
+    beforeEach(() => {
+      PaginationWrapper = wrapperFactory(pureTable, constants);
+      wrapper = shallow(<PaginationWrapper { ...props } />);
+      instance = wrapper.instance();
+      const fragment = instance.render();
+      wrapper = shallow(<div>{ fragment }</div>);
+    });
+
+    it('should rendering correctly', () => {
+      expect(wrapper.length).toBe(1);
+    });
+
+    it('should initializing state correctly', () => {
+      expect(instance.state.currPage).toBeDefined();
+      expect(instance.state.currPage).toEqual(constants.PAGE_START_INDEX);
+      expect(instance.state.currSizePerPage).toBeDefined();
+      expect(instance.state.currSizePerPage).toEqual(constants.SIZE_PER_PAGE_LIST[0]);
+    });
+
+    it('should rendering BootstraTable correctly', () => {
+      const table = wrapper.find(BootstrapTable);
+      expect(table.length).toBe(1);
+      expect(table.prop('data').length).toEqual(instance.state.currSizePerPage);
+    });
+
+    it('should rendering Pagination correctly', () => {
+      const pagination = wrapper.find(Pagination);
+      expect(pagination.length).toBe(1);
+      expect(pagination.prop('dataSize')).toEqual(props.store.getDataSize());
+      expect(pagination.prop('currPage')).toEqual(instance.state.currPage);
+      expect(pagination.prop('currSizePerPage')).toEqual(instance.state.currSizePerPage);
+      expect(pagination.prop('onPageChange')).toEqual(instance.handleChangePage);
+      expect(pagination.prop('onSizePerPageChange')).toEqual(instance.handleChangeSizePerPage);
+      expect(pagination.prop('sizePerPageList')).toEqual(constants.SIZE_PER_PAGE_LIST);
+      expect(pagination.prop('paginationSize')).toEqual(constants.PAGINATION_SIZE);
+      expect(pagination.prop('pageStartIndex')).toEqual(constants.PAGE_START_INDEX);
+      expect(pagination.prop('withFirstAndLast')).toEqual(constants.With_FIRST_AND_LAST);
+      expect(pagination.prop('alwaysShowAllBtns')).toEqual(constants.SHOW_ALL_PAGE_BTNS);
+      expect(pagination.prop('firstPageText')).toEqual(constants.FIRST_PAGE_TEXT);
+      expect(pagination.prop('prePageText')).toEqual(constants.PRE_PAGE_TEXT);
+      expect(pagination.prop('nextPageText')).toEqual(constants.NEXT_PAGE_TEXT);
+      expect(pagination.prop('lastPageText')).toEqual(constants.LAST_PAGE_TEXT);
+    });
+  });
+
+  describe('when options.pageStartIndex is defined', () => {
+    const props = createTableProps({ options: { pageStartIndex: -1 } });
+    beforeEach(() => {
+      PaginationWrapper = wrapperFactory(pureTable, constants);
+      wrapper = shallow(<PaginationWrapper { ...props } />);
+      instance = wrapper.instance();
+      const fragment = instance.render();
+      wrapper = shallow(<div>{ fragment }</div>);
+    });
+
+    it('should setting correct state.currPage', () => {
+      expect(instance.state.currPage).toEqual(props.options.pageStartIndex);
+    });
+
+    it('should rendering Pagination correctly', () => {
+      const pagination = wrapper.find(Pagination);
+      expect(wrapper.length).toBe(1);
+      expect(pagination.length).toBe(1);
+      expect(pagination.prop('pageStartIndex')).toEqual(props.options.pageStartIndex);
+    });
+  });
+
+  describe('when options.sizePerPageList is defined', () => {
+    const props = createTableProps({ options: { sizePerPageList: [10, 40] } });
+    beforeEach(() => {
+      PaginationWrapper = wrapperFactory(pureTable, constants);
+      wrapper = shallow(<PaginationWrapper { ...props } />);
+      instance = wrapper.instance();
+      const fragment = instance.render();
+      wrapper = shallow(<div>{ fragment }</div>);
+    });
+
+    it('should rendering Pagination correctly', () => {
+      const pagination = wrapper.find(Pagination);
+      expect(wrapper.length).toBe(1);
+      expect(pagination.length).toBe(1);
+      expect(pagination.prop('sizePerPageList')).toEqual(props.options.sizePerPageList);
+    });
+  });
+
+  describe('when options.paginationSize is defined', () => {
+    const props = createTableProps({ options: { paginationSize: 10 } });
+    beforeEach(() => {
+      PaginationWrapper = wrapperFactory(pureTable, constants);
+      wrapper = shallow(<PaginationWrapper { ...props } />);
+      instance = wrapper.instance();
+      const fragment = instance.render();
+      wrapper = shallow(<div>{ fragment }</div>);
+    });
+
+    it('should rendering Pagination correctly', () => {
+      const pagination = wrapper.find(Pagination);
+      expect(wrapper.length).toBe(1);
+      expect(pagination.length).toBe(1);
+      expect(pagination.prop('paginationSize')).toEqual(props.options.paginationSize);
+    });
+  });
+
+  describe('when options.withFirstAndLast is defined', () => {
+    const props = createTableProps({ options: { withFirstAndLast: false } });
+    beforeEach(() => {
+      PaginationWrapper = wrapperFactory(pureTable, constants);
+      wrapper = shallow(<PaginationWrapper { ...props } />);
+      instance = wrapper.instance();
+      const fragment = instance.render();
+      wrapper = shallow(<div>{ fragment }</div>);
+    });
+
+    it('should rendering Pagination correctly', () => {
+      const pagination = wrapper.find(Pagination);
+      expect(wrapper.length).toBe(1);
+      expect(pagination.length).toBe(1);
+      expect(pagination.prop('withFirstAndLast')).toEqual(props.options.withFirstAndLast);
+    });
+  });
+
+  describe('when options.alwaysShowAllBtns is defined', () => {
+    const props = createTableProps({ options: { alwaysShowAllBtns: true } });
+    beforeEach(() => {
+      PaginationWrapper = wrapperFactory(pureTable, constants);
+      wrapper = shallow(<PaginationWrapper { ...props } />);
+      instance = wrapper.instance();
+      const fragment = instance.render();
+      wrapper = shallow(<div>{ fragment }</div>);
+    });
+
+    it('should rendering Pagination correctly', () => {
+      const pagination = wrapper.find(Pagination);
+      expect(wrapper.length).toBe(1);
+      expect(pagination.length).toBe(1);
+      expect(pagination.prop('alwaysShowAllBtns')).toEqual(props.options.alwaysShowAllBtns);
+    });
+  });
+
+  describe('when options.firstPageText is defined', () => {
+    const props = createTableProps({ options: { firstPageText: '1st' } });
+    beforeEach(() => {
+      PaginationWrapper = wrapperFactory(pureTable, constants);
+      wrapper = shallow(<PaginationWrapper { ...props } />);
+      instance = wrapper.instance();
+      const fragment = instance.render();
+      wrapper = shallow(<div>{ fragment }</div>);
+    });
+
+    it('should rendering Pagination correctly', () => {
+      const pagination = wrapper.find(Pagination);
+      expect(wrapper.length).toBe(1);
+      expect(pagination.length).toBe(1);
+      expect(pagination.prop('firstPageText')).toEqual(props.options.firstPageText);
+    });
+  });
+
+  describe('when options.prePageText is defined', () => {
+    const props = createTableProps({ options: { prePageText: 'PRE' } });
+    beforeEach(() => {
+      PaginationWrapper = wrapperFactory(pureTable, constants);
+      wrapper = shallow(<PaginationWrapper { ...props } />);
+      instance = wrapper.instance();
+      const fragment = instance.render();
+      wrapper = shallow(<div>{ fragment }</div>);
+    });
+
+    it('should rendering Pagination correctly', () => {
+      const pagination = wrapper.find(Pagination);
+      expect(wrapper.length).toBe(1);
+      expect(pagination.length).toBe(1);
+      expect(pagination.prop('prePageText')).toEqual(props.options.prePageText);
+    });
+  });
+
+  describe('when options.nextPageText is defined', () => {
+    const props = createTableProps({ options: { nextPageText: 'NEXT' } });
+    beforeEach(() => {
+      PaginationWrapper = wrapperFactory(pureTable, constants);
+      wrapper = shallow(<PaginationWrapper { ...props } />);
+      instance = wrapper.instance();
+      const fragment = instance.render();
+      wrapper = shallow(<div>{ fragment }</div>);
+    });
+
+    it('should rendering Pagination correctly', () => {
+      const pagination = wrapper.find(Pagination);
+      expect(wrapper.length).toBe(1);
+      expect(pagination.length).toBe(1);
+      expect(pagination.prop('nextPageText')).toEqual(props.options.nextPageText);
+    });
+  });
+
+  describe('when options.lastPageText is defined', () => {
+    const props = createTableProps({ options: { lastPageText: 'nth' } });
+    beforeEach(() => {
+      PaginationWrapper = wrapperFactory(pureTable, constants);
+      wrapper = shallow(<PaginationWrapper { ...props } />);
+      instance = wrapper.instance();
+      const fragment = instance.render();
+      wrapper = shallow(<div>{ fragment }</div>);
+    });
+
+    it('should rendering Pagination correctly', () => {
+      const pagination = wrapper.find(Pagination);
+      expect(wrapper.length).toBe(1);
+      expect(pagination.length).toBe(1);
+      expect(pagination.prop('lastPageText')).toEqual(props.options.lastPageText);
+    });
+  });
+
+  describe('handleChangePage', () => {
+    const newPage = 3;
+    const props = createTableProps({ options: { onPageChange: sinon.stub() } });
+    beforeEach(() => {
+      PaginationWrapper = wrapperFactory(pureTable, constants);
+      wrapper = shallow(<PaginationWrapper { ...props } />);
+      instance = wrapper.instance();
+      instance.handleChangePage(newPage);
+    });
+
+    afterEach(() => {
+      props.options.onPageChange.reset();
+    });
+
+    it('should setting state.currPage correctly', () => {
+      expect(instance.state.currPage).toEqual(newPage);
+    });
+
+    it('should calling options.onPageChange correctly when it is defined', () => {
+      const { onPageChange } = props.options;
+      expect(onPageChange.calledOnce).toBeTruthy();
+      expect(onPageChange.calledWith(newPage, instance.state.currSizePerPage)).toBeTruthy();
+    });
+  });
+
+  describe('handleChangeSizePerPage', () => {
+    const newPage = 2;
+    const newSizePerPage = 30;
+    const props = createTableProps({ options: { onSizePerPageChange: sinon.stub() } });
+    beforeEach(() => {
+      PaginationWrapper = wrapperFactory(pureTable, constants);
+      wrapper = shallow(<PaginationWrapper { ...props } />);
+      instance = wrapper.instance();
+      instance.handleChangeSizePerPage(newSizePerPage, newPage);
+    });
+
+    afterEach(() => {
+      props.options.onSizePerPageChange.reset();
+    });
+
+    it('should setting state.currPage and state.currSizePerPage correctly', () => {
+      expect(instance.state.currPage).toEqual(newPage);
+      expect(instance.state.currSizePerPage).toEqual(newSizePerPage);
+    });
+
+    it('should calling options.onSizePerPageChange correctly when it is defined', () => {
+      const { onSizePerPageChange } = props.options;
+      expect(onSizePerPageChange.calledOnce).toBeTruthy();
+      expect(onSizePerPageChange.calledWith(newSizePerPage, newPage)).toBeTruthy();
+    });
+  });
+});
