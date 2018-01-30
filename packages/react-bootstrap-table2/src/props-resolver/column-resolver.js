8bfbc14bd928ca8c81a7766086727652f8d0b774
AllenFang <ayu780129@hotmail.com>
2018-01-31 00:01:04 +0800

    fix #185

--- packages/react-bootstrap-table2/src/props-resolver/column-resolver.js
+++ packages/react-bootstrap-table2/src/props-resolver/column-resolver.js
@@ -1,6 +1,6 @@
 export default ExtendBase =>
   class ColumnResolver extends ExtendBase {
     visibleColumnSize() {
-      return this.props.columns.length;
+      return this.props.columns.filter(c => !c.hidden).length;
     }
   };
