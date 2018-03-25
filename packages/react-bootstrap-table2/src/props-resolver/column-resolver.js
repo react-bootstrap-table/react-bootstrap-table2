ed3b3ff98b3dbf1c5e9488baddd7fd97ce6d758a
Patrick O'Meara <pat@fixd.io>
2018-03-25 22:22:33 +1100

    noDataIndication
    * use the correct amount of cells when the first row is select
    * storybook added for development, not necessary in docs
    
    fixes react-bootstrap-table/react-bootstrap-table2#264

--- packages/react-bootstrap-table2/src/props-resolver/column-resolver.js
+++ packages/react-bootstrap-table2/src/props-resolver/column-resolver.js
@@ -1,6 +1,11 @@
 export default ExtendBase =>
   class ColumnResolver extends ExtendBase {
-    visibleColumnSize() {
-      return this.props.columns.filter(c => !c.hidden).length;
+    visibleColumnSize(includeSelectColumn = true) {
+      const columnLen = this.props.columns.filter(c => !c.hidden).length;
+      if (!includeSelectColumn) return columnLen;
+      if (this.props.selectRow && !this.props.selectRow.hideSelectColumn) {
+        return columnLen + 1;
+      }
+      return columnLen;
     }
   };
