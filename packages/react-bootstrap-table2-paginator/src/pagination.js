b282bbaef493970b5c10d649636fa100f4d77dc8
Anuraag Gupta <anuraagg.kval3@gmail.com>
2018-04-19 23:35:56 +0530

    Updated pagination.js as per to linting standards

--- packages/react-bootstrap-table2-paginator/src/pagination.js
+++ packages/react-bootstrap-table2-paginator/src/pagination.js
@@ -101,8 +101,9 @@ class Pagination extends pageResolver(Component) {
       'col-md-6 col-xs-6 col-sm-6 col-lg-6', {
         'react-bootstrap-table-pagination-list-hidden': (hidePageListOnlyOnePage && totalPages === 1)
       });
+    const style = this.props.style ? this.props.style : {};
     return (
-      <div className="row react-bootstrap-table-pagination">
+      <div style={ style } className="row react-bootstrap-table-pagination">
         <div className="col-md-6 col-xs-6 col-sm-6 col-lg-6">
           {
             sizePerPageList.length > 1 && !hideSizePerPage ?
@@ -119,7 +120,7 @@ class Pagination extends pageResolver(Component) {
           }
         </div>
         <div className={ pageListClass }>
-          <PaginationList pages={ pages } onPageChange={ this.handleChangePage } />
+          <PaginationList pages={ pages } onPageChange={ this.handleChangePage }/>
         </div>
       </div>
     );
@@ -127,6 +128,7 @@ class Pagination extends pageResolver(Component) {
 }
 
 Pagination.propTypes = {
+  style: PropTypes.object,
   dataSize: PropTypes.number.isRequired,
   sizePerPageList: PropTypes.array.isRequired,
   currPage: PropTypes.number.isRequired,
