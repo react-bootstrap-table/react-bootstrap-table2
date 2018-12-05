e041a3d73687a4e5e01ad0339f1e500f46f1d193
Vincent Degroote <1230888+vinzentt@users.noreply.github.com>
2018-12-05 10:55:04 +0100

    revert not needed changes

--- packages/react-bootstrap-table2-example/examples/row-expand/non-expandable-rows.js
+++ packages/react-bootstrap-table2-example/examples/row-expand/non-expandable-rows.js
@@ -25,7 +25,6 @@ const expandRow = {
       <p>expandRow.renderer callback will pass the origin row object to you</p>
     </div>
   ),
-  showExpandColumn: true,
   nonExpandable: [1, 3]
 };
 
@@ -51,7 +50,6 @@ const expandRow = {
       <p>expandRow.renderer callback will pass the origin row object to you</p>
     </div>
   ),
-  showExpandColumn: true,
   nonExpandable: [1, 3]
 };
 
