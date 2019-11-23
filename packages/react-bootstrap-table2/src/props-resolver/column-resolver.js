4a340c714bb6006a30c447eeba4fd44ceca1e379
AllenFang <ayu780129@hotmail.com>
2019-11-23 14:35:45 +0800

    fix #1168

--- packages/react-bootstrap-table2/src/props-resolver/column-resolver.js
+++ packages/react-bootstrap-table2/src/props-resolver/column-resolver.js
@@ -1,7 +1,13 @@
 export default ExtendBase =>
   class ColumnResolver extends ExtendBase {
     visibleColumnSize(includeSelectColumn = true) {
-      let columnLen = this.props.columns.filter(c => !c.hidden).length;
+      let columnLen;
+      if (this.props.columnToggle && this.props.columnToggle.toggles) {
+        const columns = this.props.columnToggle.toggles;
+        columnLen = Object.keys(columns).filter(name => columns[name]).length;
+      } else {
+        columnLen = this.props.columns.filter(c => !c.hidden).length;
+      }
       if (!includeSelectColumn) return columnLen;
       if (this.props.selectRow && !this.props.selectRow.hideSelectColumn) {
         columnLen += 1;
