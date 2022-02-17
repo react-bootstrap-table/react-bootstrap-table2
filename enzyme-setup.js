3b02f84fa170dd5f39d7d678c9140953235ec9ed
Bruno Camarneiro <camarneirobruno@gmail.com>
2022-02-17 12:08:58 +0000

    update react dependency to 17.0.2

--- enzyme-setup.js
+++ enzyme-setup.js
@@ -1,5 +1,5 @@
-import Adapter from 'enzyme-adapter-react-16.3';
-import { configure } from 'enzyme';
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { configure } from "enzyme";
 
 const configureEnzyme = () => {
   configure({ adapter: new Adapter() });
