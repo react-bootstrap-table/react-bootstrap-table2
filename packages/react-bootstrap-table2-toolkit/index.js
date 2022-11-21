0b1189d77fa41839d5cf7c1b50f678177ff6e9bd
Mirosław Michalski <mirek@montsoft.pl>
2022-11-21 21:35:06 +0100

    disable search

--- packages/react-bootstrap-table2-toolkit/index.js
+++ packages/react-bootstrap-table2-toolkit/index.js
@@ -3,6 +3,6 @@ import ToolkitProvider from './provider';
 
 export default ToolkitProvider;
 export const ToolkitContext = Context;
-export { default as Search } from './src/search';
+// export { default as Search } from './src/search';
 export { default as CSVExport } from './src/csv';
 export { default as ColumnToggle } from './src/column-toggle';
